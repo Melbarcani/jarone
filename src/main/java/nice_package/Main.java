5823bed9832983db05cdb82ff7592430b14ef6bd
Mohamed EL BARCANI <melbarcani@assetsjapan.com>
2022-10-24 08:11:01 +0200

    first commit

---
+++ src/main/java/nice_package/Main.java
@@ -0,0 +1,13 @@
+package nice_package;
+
+public class Main {
+    public static void main(String[] args) {
+        var r = computeSum(1, 2);
+        System.out.println(r);
+    }
+
+    public static Integer computeSum(Integer number, Integer
+            number2) {
+        return number + number2;
+    }
+}
